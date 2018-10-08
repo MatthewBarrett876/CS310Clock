7ec423dabffa7b24aa55ad1fe0c5d84bc31f4138
Nathan Butler <nathanielbutler94@gmail.com>
2018-10-08 12:13:12 -0500

    Creating the empty classes

--- src/teamproject/TeamProject.java
+++ src/teamproject/TeamProject.java
@@ -15,7 +15,7 @@ public class TeamProject {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
