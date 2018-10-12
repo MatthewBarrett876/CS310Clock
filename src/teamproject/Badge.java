00f026b6343bff2d0281a0b166b55467bb9e2d54
Steven Ford <sford5@stu.jsu.edu>
2018-10-11 21:04:56 -0500

    Created the Badge class.

--- src/teamproject/Badge.java
+++ src/teamproject/Badge.java
@@ -1,14 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package teamproject;
-
-/**
- *
- * @author Nathan
- */
 public class Badge {
+    //instance fields
+    private String id;
+    private String name;
+    //constructor
+    public Badge(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    //methods
+    public String getID() {return id;}
+    public String getName() {return name;}
+    public void setID(String i) {
+        this.id = i;
+    }
+    public void setName(String n) {
+        this.name = n;
+    }
+    //toString()
+    @Override
+    public String toString() {
+        return "#" + this.id + " " + '(' + this.name + ')';
+    }
+    
+    
     
 }
