77c41b185c1b63d5ed81450493f17b3f3b2dba87
Nathan Butler <nathanielbutler94@gmail.com>
2018-10-13 02:46:26 -0500

    Feature 1 TASDatabase class finished.

--- src/teamproject/TASDatabase.java
+++ src/teamproject/TASDatabase.java
@@ -28,7 +28,6 @@ public class TASDatabase {
                 Class.forName("com.mysql.jdbc.Driver").newInstance();
             }
             
-            
             catch (Exception e) 
             {
             System.err.println("Unable to connect to the database");
