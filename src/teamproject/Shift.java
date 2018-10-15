32715fc6afb2bcd3902cbeecc3e803f0a8be9a8f
Nathan Butler <nathanielbutler94@gmail.com>
2018-10-15 12:04:07 -0500

    Revert ""
    This reverts commit d330fc9a8b1a9d7eafd81e15c268166b9cfb06db.

--- src/teamproject/Shift.java
+++ src/teamproject/Shift.java
@@ -1,33 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package teamproject;
 
-import java.time.LocalTime;
-
+/**
+ *
+ * @author Nathan
+ */
 public class Shift {
-    private long start = 0;
-    private long stop = 0;
     
-public void LocalTime (int hour, int minute, int second){
-    LocalTime shift1start = LocalTime.of(07,00,00);
-    LocalTime shift1stop = LocalTime.of(15,30,00);
-    LocalTime shift2start = LocalTime.of(12,00,00);
-    LocalTime shift2stop = LocalTime.of(20,30,00);
-    LocalTime shift1L = LocalTime.of(12,00,00);
-    LocalTime shift1LS = LocalTime.of(12,30,00);
-    LocalTime shift2L = LocalTime.of(16,30,00);    
-    LocalTime shift2LS = LocalTime.of(17,00,00);
-    LocalTime shift1EL = LocalTime.of(11,30,00);
-    LocalTime shift1ELS = LocalTime.of(12,00,00);
-    
-}
-public void Start(){    this.start = System.currentTimeMillis();}
-public void Stop(){    this.stop = System.currentTimeMillis();}
-public long getStart(){    return this.start; }
-public long getStop(){    return this.stop;}
-public void Shift1(){}
-public void Shift2(){}
-public void Interval(){}
-public void GradePeriod(){}
-public void Dock(){
-}
-
 }
